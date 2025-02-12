89cc29716ebce1a5ea0648031961593de92552f5
DaanAdr <parsa1815@outlook.com>
2025-02-12 11:24:15 +0100

    Added initial project files

---
+++ Organ Pipe Foot Model Generator/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Organ_Pipe_Foot_Model_Generator;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
+
