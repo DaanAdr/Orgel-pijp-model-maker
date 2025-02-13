7c19568b89e1db2c8da6a7382fddc757099c9982
DaanAdr <parsa1815@outlook.com>
2025-02-13 11:01:06 +0100

    Added unit test for the slanted side length calculation

---
+++ Organ Pipe Foot Model Generator Tests/UnitTests/PipeFootMeasurementsTests.cs
@@ -0,0 +1,25 @@
+﻿using Organ_Pipe_Foot_Model_Generator.Entities;
+
+namespace Organ_Pipe_Foot_Model_Generator_Tests.UnitTests
+{
+    public class PipeFootMeasurementsTests
+    {
+        public readonly double _topDiameter = 50;
+        public readonly double _bottonDiameter = 150;
+        public readonly double _height = 200;
+
+        [Fact]
+        public void CalculateLengthSlantedSide_Returns206dot2()
+        {
+            //Arrange
+            PipeFootMeasurements measurements = new PipeFootMeasurements(_topDiameter, _bottonDiameter, _height);
+
+            //Act
+            double lengthSlantedSide = measurements.CalculateLengthSlantedSide();
+            double roundedLength = Math.Round(lengthSlantedSide, 1);
+
+            //Assert
+            Assert.Equal(expected: 206.2, actual: roundedLength);
+        }
+    }
+}
