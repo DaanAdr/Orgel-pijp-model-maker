9456da034de62f75abe7cbefa610feb4d3a750e4
DaanAdr <parsa1815@outlook.com>
2025-02-19 11:24:55 +0100

    Added all assertions for CalculateMeasurements_TopAndBottomDiametersAreInnerDiameters

--- Organ Pipe Foot Model Generator Tests/UnitTests/PipeFootMeasurementsTests.cs
+++ Organ Pipe Foot Model Generator Tests/UnitTests/PipeFootMeasurementsTests.cs
@@ -27,14 +27,27 @@ namespace Organ_Pipe_Foot_Model_Generator_Tests.UnitTests
             Assert.Equal(expected: 87.3, actual: measurements.CornerInDegrees);
 
             // Model
+            // Bottomline
             Assert.Equal(new CSMath.XYZ(x: 100, y: 100, z: 0), pipeFootTemplate.Bottomline.StartPoint);
             Assert.Equal(new CSMath.XYZ(x: 306.2, y: 100, z: 0), pipeFootTemplate.Bottomline.EndPoint);
 
+            // Small Arc
             Assert.Equal(0, pipeFootTemplate.SmallArc.StartAngle);
-            var radians = 87.3 * (Math.PI / 180);
-            Assert.Equal(radians, pipeFootTemplate.SmallArc.EndAngle); //Might be different due to radians
+            var radiansSmallArc = 87.3 * (Math.PI / 180);
+            Assert.Equal(radiansSmallArc, pipeFootTemplate.SmallArc.EndAngle);
             Assert.Equal(new CSMath.XYZ(x: -3.1, y: 100, z: 0), pipeFootTemplate.SmallArc.Center);
             Assert.Equal(103.1, pipeFootTemplate.SmallArc.Radius);
+
+            // Large Arc
+            Assert.Equal(0, pipeFootTemplate.LargeArc.StartAngle);
+            var radiansLargeArc = 87.3 * (Math.PI / 180);
+            Assert.Equal(radiansLargeArc, pipeFootTemplate.LargeArc.EndAngle);
+            Assert.Equal(new CSMath.XYZ(x: -3.1, y: 100, z: 0), pipeFootTemplate.LargeArc.Center);
+            Assert.Equal(309.3, pipeFootTemplate.LargeArc.Radius);
+
+            // Slanted line
+            Assert.Equal(new CSMath.XYZ(x: 1.8, y: 203, z: 0), pipeFootTemplate.Bottomline.StartPoint);
+            Assert.Equal(new CSMath.XYZ(x: 11.5, y: 409, z: 0), pipeFootTemplate.Bottomline.EndPoint);
         }
 
         [Fact]
