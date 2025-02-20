c3406a69113b7445b248059c781c8dac52ff459f
DaanAdr <parsa1815@outlook.com>
2025-02-20 16:23:06 +0100

    Added comments to LabiaalPijpExcel and PipeFoorMeasurements

--- Organ Pipe Foot Model Generator/Entities/LabiaalPijpExcel.cs
+++ Organ Pipe Foot Model Generator/Entities/LabiaalPijpExcel.cs
@@ -3,34 +3,21 @@
 
 namespace Organ_Pipe_Foot_Model_Generator.Entities
 {
+    /// <summary>
+    /// This class it to be used when reading Excel files for Labiaal pijpen
+    /// </summary>
     public class LabiaalPijpExcel
     {
         [Name("toets")]
         public required string Key { get; set; }
-        //public string corpl { get; set; }
 
         [Name("uitw diam")]
         public required double TopDiameter { get; set; }
-        //public string labbr { get; set; }
-        //public string kernd { get; set; }
-        //public string kernf { get; set; }
-        //[Name("pl.brdt")]
-        //public string PlaatBreedte { get; set; }
-        //[Name("lab %")]
-        //public string LabPercentage { get; set; }
-        //public string opsn { get; set; }
-        //public string voetg { get; set; }
         [Name("vt.lngt")]
         public required double Height { get; set; }
         [Name("pl.br.vt")]
         public required double PlateWidthFoot { get; set; }
         [Name("wd.vt")]
         public required double MetalThickness { get; set; }
-        //[Name("wd.bo")]
-        //public string WijdteBoven { get; set; }
-        //[Name("wd.on")]
-        //public string WijdteOnder { get; set; }
-        //[Name("wel.lngt")]
-        //public string WelLengte { get; set; }
     }   
 }
--- Organ Pipe Foot Model Generator/Entities/PipeFootMeasurements.cs
+++ Organ Pipe Foot Model Generator/Entities/PipeFootMeasurements.cs
@@ -2,8 +2,6 @@
 {
     public class PipeFootMeasurements
     {
-        public double TopDiameter { get; }
-        public double BottomDiameter { get; }
         public double Height { get; }
         public double LengthSlantedSide { get; private set; }
         public double LengthBottomDiameter { get; private set; }
@@ -12,24 +10,37 @@
         public double LargeRadius { get; private set; }
         public double CornerInDegrees { get; private set; }
 
+        /// <summary>
+        /// Calculates all values necessary to make a PipeFootTemplate
+        /// </summary>
+        /// <param name="topDiameter">The length of the top of the frustum. Labeled T in Images/frustum.png</param>
+        /// <param name="bottomDiameter">The length of the bottom of the frustum. Labeled B in Images/frustum.png</param>
+        /// <param name="height">The height of the frustum. Labeled H in Images/frustum.png</param>
+        /// <param name="metalThickness">How thick the sheet metal for the pipe is, this has to be subtracted (twice) from the top- and bottomdiameter if the measurements are for the outerdiameter of the pipes</param>
         public PipeFootMeasurements(double topDiameter, double bottomDiameter, double height, double metalThickness)
         {
-            TopDiameter = Math.Round(topDiameter - (metalThickness * 2), 1);
-            BottomDiameter = Math.Round(bottomDiameter - (metalThickness * 2), 1);
+            topDiameter = Math.Round(topDiameter - (metalThickness * 2), 1);
+            bottomDiameter = Math.Round(bottomDiameter - (metalThickness * 2), 1);
             Height = Math.Round(height, 1);
 
-            LengthSlantedSide = CalculateLengthSlantedSide();
-            LengthBottomDiameter = CalculateLengthBottomDiameter();
-            LengthTopDiameter = CalculateLengthTopDiameter();
+            LengthSlantedSide = CalculateLengthSlantedSide(topDiameter, bottomDiameter);
+            LengthBottomDiameter = CalculateLengthBottomDiameter(bottomDiameter);
+            LengthTopDiameter = CalculateLengthTopDiameter(topDiameter);
             SmallRadius = CalculateSmallRadius();
             LargeRadius = CalculateLargeRadius();
             CornerInDegrees = CalculateCornerDegrees();
         }
 
-        private double CalculateLengthSlantedSide()
+        /// <summary>
+        /// Calculates the length for the slanted sides of the frustum. Labeled R in Images/frustum.png
+        /// </summary>
+        /// <param name="topDiameter">The length of the top of the frustum. Labeled T in Images/frustum.png</param>
+        /// <param name="bottomDiameter">The length of the bottom of the frustum. Labeled B in Images/frustum.png</param>
+        /// <returns></returns>
+        private double CalculateLengthSlantedSide(double topDiameter, double bottomDiameter)
         {
-            double topDiameterHalved = TopDiameter / 2;
-            double bottomDiameterHalved = BottomDiameter / 2;
+            double topDiameterHalved = topDiameter / 2;
+            double bottomDiameterHalved = bottomDiameter / 2;
 
             double halvedTopMinusHalvedBottom = topDiameterHalved - bottomDiameterHalved;
             double halvedTopMinusHalvedBottomSquared = halvedTopMinusHalvedBottom * halvedTopMinusHalvedBottom;
@@ -40,18 +51,32 @@
             return Math.Round(lengthSlantedSide, 1); ;
         }
 
-        private double CalculateLengthBottomDiameter()
+        /// <summary>
+        /// Calculates the length of the SmallArc. Labeled L in Images/template.png
+        /// </summary>
+        /// <param name="bottomDiameter">The length of the bottom of the frustum. Labeled B in Images/frustum.png</param>
+        /// <returns></returns>
+        private double CalculateLengthBottomDiameter(double bottomDiameter)
         {
-            double bottomDiameterTimesPi = BottomDiameter * Math.PI;
+            double bottomDiameterTimesPi = bottomDiameter * Math.PI;
             return Math.Round(bottomDiameterTimesPi, 1);
         }
 
-        private double CalculateLengthTopDiameter()
+        /// <summary>
+        /// Calculates the length of the LargeArc. Labeled M in Images/template.png
+        /// </summary>
+        /// <param name="topDiameter">The length of the top of the frustum. Labeled T in Images/frustum.png</param>
+        /// <returns></returns>
+        private double CalculateLengthTopDiameter(double topDiameter)
         {
-            double topDiameterTimesPi = TopDiameter * Math.PI;
+            double topDiameterTimesPi = topDiameter * Math.PI;
             return Math.Round(topDiameterTimesPi, 1);
         }
 
+        /// <summary>
+        /// Calculates the radius (straal in Dutch) for the SmallArc. Labeled P in Images/template.png
+        /// </summary>
+        /// <returns></returns>
         private double CalculateSmallRadius()
         {
             double lengthInnerDiameterTimesLengthSlantedSide = LengthBottomDiameter * LengthSlantedSide;
@@ -60,12 +85,20 @@
             return Math.Round(divide, 1);
         }
 
+        /// <summary>
+        /// Calculates the radius (straal in Dutch) for the LargeArc. Labeled Q in Images/template.png
+        /// </summary>
+        /// <returns></returns>
         private double CalculateLargeRadius()
         {
             double smallRadiusPlusLengthSlantedSide = SmallRadius + LengthSlantedSide;
             return Math.Round(smallRadiusPlusLengthSlantedSide, 1);
         }
 
+        /// <summary>
+        /// Calculates how many degrees the corner is at A in Images/template.png
+        /// </summary>
+        /// <returns></returns>
         private double CalculateCornerDegrees()
         {
             double lengthInnerDiameterDividedBySmallRadius = LengthBottomDiameter / SmallRadius;
