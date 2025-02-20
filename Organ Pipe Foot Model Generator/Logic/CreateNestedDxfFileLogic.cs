cff923130e9eddda4c2fd149ad8795e5654a343d
DaanAdr <parsa1815@outlook.com>
2025-02-20 10:39:53 +0100

    Created a function that should group all pipes fro mthe excel file

--- Organ Pipe Foot Model Generator/Logic/CreateNestedDxfFileLogic.cs
+++ Organ Pipe Foot Model Generator/Logic/CreateNestedDxfFileLogic.cs
@@ -9,13 +9,73 @@ namespace Organ_Pipe_Foot_Model_Generator.Logic
 {
     public static class CreateNestedDxfFileLogic
     {
-        public static void CreateNestedDxfFile()
+        public static void CreateNestedDxfFile(List<LabiaalPijpExcel> excelPipes)
         {
             // Group all pipes by octave
 
             // Determine Y offset for next octave
 
             // Convert list of excel labiaal pijpen to list of PipeFootTemplate
+        }
+
+        private static Dictionary<string, List<LabiaalPijpExcel>> GroupLabiaalPijpExcelByOctave(List<LabiaalPijpExcel> excelPipes)
+        {
+            List<LabiaalPijpExcel> major = new List<LabiaalPijpExcel>();
+            List<LabiaalPijpExcel> minor = new List<LabiaalPijpExcel>();
+            List<LabiaalPijpExcel> first = new List<LabiaalPijpExcel>();
+            List<LabiaalPijpExcel> second = new List<LabiaalPijpExcel>();
+            List<LabiaalPijpExcel> third = new List<LabiaalPijpExcel>();
+            List<LabiaalPijpExcel> fourth = new List<LabiaalPijpExcel>();
+
+            // Loop through all pipes
+            foreach (LabiaalPijpExcel excelPipe in excelPipes)
+            {
+                // Check if first char is upper case, if yes at to "MAJOR"
+                if (char.IsUpper(excelPipe.Key[0]))
+                {
+                    major.Add(excelPipe);
+                }
+
+                // Check if last char is a number
+                char lastCharOfKey = excelPipe.Key[excelPipe.Key.Length - 1];
+                int number;
+
+                if (int.TryParse(lastCharOfKey.ToString(), out number))
+                {
+                    switch(number)
+                    {
+                        case 1:
+                            first.Add(excelPipe);
+                            break;
+                        case 2:
+                            second.Add(excelPipe);
+                            break;
+                        case 3:
+                            third.Add(excelPipe);
+                            break;
+                        case 4:
+                            fourth.Add(excelPipe);
+                            break;
+                    }
+                        
+                }
+
+                // Add remaining to "MINOR"
+                minor.Add(excelPipe);
+            }
+
+            // Create a dictionary to hold the lists
+            Dictionary<string, List<LabiaalPijpExcel>> octaves = new Dictionary<string, List<LabiaalPijpExcel>>
+            {
+                { "MAJOR", major },
+                { "MINOR", minor },
+                { "FIRST", first },
+                { "SECOND", second },
+                { "THIRD", third },
+                { "FOURTH", fourth }
+            };
+
+            return octaves;
         }
 
         private List<PipeFootTemplate> ConvertLabiaalPijpExcelToTemplateForOctave(List<LabiaalPijpExcel> excelPipes)
